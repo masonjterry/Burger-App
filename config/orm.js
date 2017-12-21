f4ec944f9dce75e61b998b89b56069f12222ebe3
unknown <masonjterry@gmail.com>
2017-12-20 23:23:32 -0600

    set up orm file

---
+++ config/orm.js
@@ -0,0 +1,66 @@
+let connection = require("./connection.js");
+
+function printQuestionMarks(num) {
+  let arr = [];
+
+  for (let i = 0; i < num; i++) {
+    arr.push("?");
+  }
+  return arr.toString();
+}
+
+function objToSql(ob) {
+  let arr = [];
+
+  for (let key in ob) {
+    let value = ob[key];
+
+    if (Object.hasOwnProperty.call(ob, key)) {
+      if (typeof value === "string" && value.indexOf(" ") >= 0) {
+        value = "'" + value + "'";
+      }
+      arr.push(key + "=" + value);
+    }
+  }
+  return arr.toString();
+}
+
+let orm = {
+  all: function(tableInput, cb) {
+    let queryString = "SELECT * FROM " + tableInput + ";";
+    connection.query(queryString, function(err, data) {
+      if (err) throw err;
+      cb(data);
+    });
+  },
+  create: function(table, cols, vals, cb) {
+    let queryString = "INSERT INTO " + table;
+    queryString += " (";
+    queryString += cols.toString();
+    queryString += ") ";
+    queryString += "VALUES (";
+    queryString += printQuestionMarks(vals.length);
+    queryString += ") ";
+
+    console.log(queryString);
+
+    connection.query(queryString, vals, function(err, datt) {
+      if (err) throw err;
+      cb(data);
+    });
+  },
+  update: function(table, objColVals, condition, cb) {
+    let queryString = "UPDATE " + table;
+    queryString += " SET ";
+    queryString += objToSql(objColVals);
+    queryString += condition;
+
+    console.log(queryString);
+    connection.query(queryString, function(err, data) {
+      if (err) throw err;
+      cb(data);
+    });
+  }
+};
+
+module.exports = orm;
