bf716b92ad7c5f84d9e9cc25426c9aeb8192386f
unknown <masonjterry@gmail.com>
2017-12-20 23:25:35 -0600

    added server.js file

---
+++ server.js
@@ -0,0 +1,19 @@
+let express = require("express");
+let app = express();
+let bodyParser = require("body-parser");
+let PORT = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({extended: false }));
+
+let exphds = require("express-handlebars");
+
+app.engine("handlebars", exphds({ defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+let routes = require("./controllers/burgerController.js");
+
+app.use("/", routes);
+
+app.listen(PORT);
