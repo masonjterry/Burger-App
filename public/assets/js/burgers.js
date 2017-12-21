9804d0885ad439d1a1da275b381dce052d7db630
unknown <masonjterry@gmail.com>
2017-12-20 23:28:42 -0600

    added js files in the public folder

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,36 @@
+$(function() {
+  $(".change-devoured").on("click", function(event) {
+    let id = $(this).data("id");
+    let newDevoured = $(this).data("newdevoured");
+    let newDevouredState = {
+      devoured: newDevoured
+    };
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: newDevouredState
+    }).then(
+      function() {
+        console.log("changed devoured to", newDevoured);
+        location.reload();
+      }
+    );
+  });
+
+  $(".create-form").on("submit", function(event) {
+    event.preventDefault();
+    let newBurger = {
+      burger_name: $("#ba").val().trim(),
+      devoured: $("[name=devoured]:checked").val().trim()
+    };
+
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger
+    }).then(
+      function() {
+        console.log("created new burger");
+        location.reload();
+      }
+    );
+  });
+});
