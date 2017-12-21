87dffab3d6115103576801921a336836b69f7786
unknown <masonjterry@gmail.com>
2017-12-20 23:22:30 -0600

    setting up connection.js page to start mysql connection

---
+++ config/connection.js
@@ -0,0 +1,14 @@
+let mysql = require("mysql");
+let connection = mysql.createConnection({
+  port: process.env.port || 3000,
+  host: "localhost",
+  user: "root",
+  password: "1234",
+  database: "burgers_db"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+});
+
+module.exports = connection;
