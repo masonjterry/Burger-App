62581eb95f2510332d021569fa80e756e3f3ca9e
unknown <masonjterry@gmail.com>
2017-12-22 17:57:07 -0600

    added jawsdb connection

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,17 @@
 let mysql = require("mysql");
-let connection = mysql.createConnection({
-  port: process.env.port || 3306,
-  host: "localhost",
-  user: "root",
-  password: "1234",
-  database: "burgers_db"
-});
+let connection;
+
+  if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+  } else {
+    connection = mysql.createConnection({
+    port: process.env.port || 3306,
+    host: "localhost",
+    user: "root",
+    password: "1234",
+    database: "burgers_db"
+  });
+};
 
 connection.connect(function(err) {
   if (err) throw err;
